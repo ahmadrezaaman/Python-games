b3fbae0d932f4cfc8a7af3237cd06ceb5df425d5
ahmadrezaaman <ahmadrezaaman20@gmail.com>
2025-11-19 22:51:44 +0330

    black jack.py

--- black jak.py
+++ black jak.py
@@ -19,13 +19,11 @@ print('''\n\t*** welcome to our casino ***\n
 ''')
 time.sleep(2)
 class Card:
-    def __init__(self, num,suit,score='0'):
-        self.score = score
-        if num in ["1","2","3","4","5","6","7","8","9","10"]:
-            self.score = int(num)
-        elif num in ["Jack", "Queen", "King"]:
+    def __init__(self, num,suit):
+        self.score = num
+        if num in ["Jack", "Queen", "King"]:
             self.score = 10
-        else:
+        elif num in ["Ace"]:
             self.score = 11
         self.num = num
         self.suit = suit
@@ -34,7 +32,7 @@ player_cards = []
 father_cards = []
 cards=[]
 for i in ["heart", "diamond", "club", "spade"]:
-    for j in ['1','2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']:
+    for j in range(1,11):
         cards.append(Card(j,i))
 random.shuffle(cards)
 for i in range(2):
@@ -44,15 +42,13 @@ for i in range(2):
     time.sleep(1)
 print(f'the father card is: {father_cards[0].num} {father_cards[0].suit}')
 time.sleep(1)
-c_d=input("do you want to continue?(y/n)")
-i=2
-sum_player_cards = 0
-while c_d in ['y','Y']:
-    player_cards.append(cards.pop())
-    print(f"your {i+1}th card is: {cards[i].num} {cards[i].suit}")
-    c_d = input("do you want to continue?(y/n)")
-    sum_player_cards += player_cards[i].score
-    i=i+1
+sum_player_cards = player_cards[0].score + player_cards[1].score
+while input('do you want to continue? (y/n)')[0].lower() == 'y':
+    x=cards.pop()
+    player_cards.append(x)
+    sum_player_cards +=x.score
+    print(f'your last card is: {x.num} {x.suit}')
+print(player_cards[0].score, player_cards[1].score, player_cards[2].score)
 print(f'sum of your scores is: {sum_player_cards}')
 if sum_player_cards > 21:
     print("sorry my frined you lose")
@@ -79,12 +75,3 @@ if sum_father_cards == sum_player_cards:
     print("sorry my frined you lose")
     print(sum_father_cards)
     exit()
-
-
-
-
-
-
-
-
-
